1e6a6cc788f899410511a54954606bdcca75bfcd
Carlos Emanuel Soares de Lima <cesl.gunner@gmail.com>
2024-09-29 23:33:40 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -47,3 +47,23 @@ $(document).ready(function(){
         }
     });
 });
+
+
+
+
+
+
+const myObserver = new IntersectionObserver( (entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('show')
+        } else{
+            entry.target.classList.remove('show')
+        }
+
+    })
+})
+
+const elements = document.querySelectorAll('.hidden')
+
+elements.forEach((element) => myObserver.observe(element))
